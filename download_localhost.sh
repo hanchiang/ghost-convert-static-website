4915146e653347efb0a6e1124060b820556a6382
Yap Han Chiang <yaphc@hotmail.com>
2018-12-01 17:23:51 +0800

    download sitemap too

--- download_localhost.sh
+++ download_localhost.sh
@@ -7,7 +7,12 @@ mkdir -p ../ghost-static-generated
 find ../ghost-static-generated -mindepth 1 ! -iname "README.md" ! -path "*.git*" -delete
 
 wget -mkEpnp $1
-mv $1/* ../ghost-static-generated
-rm -r $1
 
+# get sitemap!
+wget $1/sitemap.xml
+
+mv $1/* ../ghost-static-generated
+mv sitemap.xml ../ghost-static-generated
+
+rm -r $1
 echo "$1 downloaded!"
