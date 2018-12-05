c5176e4a4d5c74adae801321f66bce0d926c44d9
Yap Han Chiang <yaphc@hotmail.com>
2018-12-05 23:21:02 +0800

    clean up

--- replace_http_with_https.sh
+++ replace_http_with_https.sh
@@ -3,6 +3,4 @@ echo "Replacing http with https..."
 # Replace http:// with https://
 find ../ghost-static-generated -type f -iname "*" -exec sed -i "s/http:\/\//https:\/\//g" {} \;
 
-sed -i "s/\/\/yaphc\.com\/sitemap\.xsl/\/sitemap\.xsl/" ../ghost-static-generated/sitemap.xml;
-
 echo "Done!"
